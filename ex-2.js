d35ae39816e319defde35e617df66b08d4b81bdf
Gunzorla <g.gun.n.1991@gmail.com>
2024-10-20 19:47:32 +0700

    Add Exercise 1-2

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,23 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  const passingStudents = array.filter(score => score > 70).length;
+  return operation(passingStudents);
 }
 
+function passOrFail(passingCount) {
+  return passingCount >= 5 ? 'ผ่านเกณฑ์' : 'ไม่ผ่านเกณฑ์';
+}
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+function checkPassCondition(studentScores, callback) {
+  return atLeastFive(studentScores, callback);
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+
+console.log("นักเรียนห้องที่ 1: " + checkPassCondition(studentScoresRoom1, passOrFail));
+console.log("นักเรียนห้องที่ 2: " + checkPassCondition(studentScoresRoom2, passOrFail));
+console.log("นักเรียนห้องที่ 3: " + checkPassCondition(studentScoresRoom3, passOrFail));
